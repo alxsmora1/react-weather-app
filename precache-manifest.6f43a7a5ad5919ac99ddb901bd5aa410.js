75ad524a875aff0b4638ed211479d43723ce8d04
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2020-11-12 22:20:43 -0600

    Updates

---
+++ precache-manifest.6f43a7a5ad5919ac99ddb901bd5aa410.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1fca3140436df851d87bd2f1bd436c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d951c7965ec9e43eeef",
+    "url": "/static/css/2.78afd188.chunk.css"
+  },
+  {
+    "revision": "c67b919e055d2f5744d6",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "7d951c7965ec9e43eeef",
+    "url": "/static/js/2.4fa9a9d8.chunk.js"
+  },
+  {
+    "revision": "4275c1d12d633733c6fec0905104cb9a",
+    "url": "/static/js/2.4fa9a9d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c67b919e055d2f5744d6",
+    "url": "/static/js/main.845ea7b0.chunk.js"
+  },
+  {
+    "revision": "3b906a68717433f63125",
+    "url": "/static/js/runtime-main.33336675.js"
+  },
+  {
+    "revision": "16670a3d347052c18343e8e0b728eeaf",
+    "url": "/static/media/undraw_weather_app_i5sm.16670a3d.svg"
+  }
+]);
