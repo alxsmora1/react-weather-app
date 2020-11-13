ca16a879ee1aebc0f5bdba86d364330841b0ce25
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2020-11-12 22:46:44 -0600

    Added React Router Add React Router structure to App for render the WeatherBox component as default.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React from "react";
+import { HashRouter, Switch, Route } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "shards-ui/dist/css/shards.min.css"
-import './App.css';
+import "shards-ui/dist/css/shards.min.css";
+import "./App.css";
 import WeatherBox from "./components/WeatherBox";
 
 function App() {
   return (
-    <div>
-      <WeatherBox />
-    </div>
+    <HashRouter basename="/">
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return (
+              <div>
+                <WeatherBox />
+              </div>
+            );
+          }}
+        ></Route>
+      </Switch>
+    </HashRouter>
   );
 }
 
