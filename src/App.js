42e39d56ff080cf091d44ac3e25fe8f5bf02f611
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2020-11-12 21:52:10 -0600

    implemented the WeatherBox component Added the imports of shards-ui and bootstrap. Implemented the new component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
 import './App.css';
+import WeatherBox from "./components/WeatherBox";
 
 function App() {
   return (
     <div>
-      
+      <WeatherBox />
     </div>
   );
 }
